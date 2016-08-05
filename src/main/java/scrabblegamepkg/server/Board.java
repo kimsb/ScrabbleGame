cdf49c805c3f2724b8566777d42ce832ac8e674a
kimsb <kim.stephen.bovim@bekk.no>
2016-08-05 15:42:52 +0200

    sletter gamle metoder som ikke lenger er i bruk

--- src/main/java/scrabblegamepkg/server/Board.java
+++ src/main/java/scrabblegamepkg/server/Board.java
@@ -73,65 +73,6 @@ public class Board {
         }
     }
 
-    //TODO: mye i denne klasse hører kanskje ikke hjemme her, men i egen klasse som spiller for CPU...
-    //denne kan gjøres raskere, nå sjekker jeg alle felter
-    void doCrossChecks(MDAG dictionary, String alphaString) {
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 15; j++) {
-                if (charBoard[i][j] == '-') {
-                    int tilesOver = 0;
-                    int tilesUnder = 0;
-                    while(i - tilesOver != 0 && charBoard[i-(tilesOver+1)][j] != '-') {
-                        tilesOver++;
-                    }
-                    while(i + tilesUnder != 14 && charBoard[i+(tilesUnder+1)][j] != '-') {
-                        tilesUnder++;
-                    }
-                    if (tilesOver != 0 || tilesUnder != 0) {
-                        crossChecks[i][j] = "";
-                        String lettersOver = "";
-                        String lettersUnder = "";
-                        for (int k = tilesOver; k > 0; k--) {
-                            lettersOver += charBoard[i-k][j];
-                        }
-                        for (int k = 1; k <= tilesUnder; k++) {
-                            lettersUnder += charBoard[i+k][j];
-                        }
-                        //sjekker alle bokstaver i alfabetet
-                        for (int k = 0; k < 29; k++) {
-                            if (dictionary.contains(lettersOver + alphaString.charAt(k) + lettersUnder)) {
-                                crossChecks[i][j] += alphaString.charAt(k);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    void updateAnchors() {
-        if (transposed) {
-            System.out.println("Trying to update Anchors, but 'transposed' is true. Swapped?");
-        }
-
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 15; j++) {
-                isAnchor[i][j] = false;
-                if (charBoard[i][j] == '-') {
-                    if (i != 0 && charBoard[i-1][j] != '-') {
-                        isAnchor[i][j] = true;
-                    } else if (j != 0 && charBoard[i][j-1] != '-') {
-                        isAnchor[i][j] = true;
-                    } else if (i != 14 && charBoard[i+1][j] != '-') {
-                        isAnchor[i][j] = true;
-                    } else if (j != 14 && charBoard[i][j+1] != '-') {
-                        isAnchor[i][j] = true;
-                    }
-                }
-            }
-        }
-    }
-
     //TODO: legge dette ut i egen klasse (BoardConstants eller noe sånt)
     private String[][] setMultipliers() {
 
